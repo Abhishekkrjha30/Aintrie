53edf5cd283b8db39f1a138342841318d73c5661
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2024-08-31 14:28:13 +0530

    Aintrie

---
+++ tailwind.config.js
@@ -0,0 +1,80 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      animation:{
+           "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes:{
+        "loop-scroll": {
+          from:{
+            transform:"translateX(0)"
+          },
+          to:{
+            transform:"translateX(-100%)"
+          }
+        },
+
+      }
+    },
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:'20px',
+        md:'50px'
+      }
+    }
+  },
+  plugins: [],
+}
+
+
+
+
+/*
+const plugin = require('tailwindcss/plugin');
+const rotateY = plugin(function({ addUtilities }){
+  addUtilities ({
+      '.rotate-y-180': {
+        transform:"rotateY(180deg)"
+      },
+      '.-rotate-y-180': {
+        transform:"rotateY(-180deg)"
+      }
+    })
+})
+
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      animation:{
+           "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes:{
+        "loop-scroll": {
+          from:{
+            transform:"translateX(0)"
+          },
+          to:{
+            transform:"translateX(-100%)"
+          }
+        },
+
+      }
+    },
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:'20px',
+        md:'50px'
+      }
+    }
+  },
+  plugins: [rotateY],
+}
+
+
+*/
